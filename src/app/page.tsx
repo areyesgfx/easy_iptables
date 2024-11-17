d46d1b1fdbe736dcae326a15727adbb28ad1c003
Antonio Reyes <48196820+areyesgfx@users.noreply.github.com>
2024-11-16 21:18:04 -0500

    Updating styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ import { FirewallRuleGenerator } from "~/app/_components/firewallRuleGenerator";
 export default async function Home() {
 
   return (
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#255079] to-[#15202c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Easy <span className="text-[hsl(280,100%,70%)]">IP</span>Tables
+            Easy <span className="text-[hsl(202,100%,70%)]">IP</span>Tables
           </h1>
           <FirewallRuleGenerator />
 
