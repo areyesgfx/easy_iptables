d2c353dd16e7c4c402cd4039286020d4c20bec7c
Antonio Reyes <48196820+areyesgfx@users.noreply.github.com>
2024-11-16 19:56:34 -0500

    Updated trpc router with threatSourceRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "~/server/api/routers/post";
+import { threatSourceRouter } from "~/server/api/routers/threatSource";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  threatSource: threatSourceRouter,
 });
 
 // export type definition of API
